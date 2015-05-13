6da542faafbfa51940ae9e3955d484458a358fe4
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-05-14 01:47:35 +0700

    test first class constructor

--- Fs/Rotate.fs
+++ Fs/Rotate.fs
@@ -2,6 +2,33 @@
 
 open FsUnit
 open NUnit.Framework
+open System.IO
+
+// test f# 4.0 constructor
+type A(p1, p2) =
+    member val F1:string = p1
+    member val F2:string = p2
+
+// test generic propery
+type GA<'a>(p1:'a) =
+    member val F1:'a = p1
+    member this.F2<'b> (b:'b) = ()
+
+[<Test>]
+let ``Constructors as First-Class Functions``() =
+    let up1 (a:A) = a.F1.ToUpper()
+    ("Abc", "Abc") |> A |> up1 |> should equal "ABC"
+
+    let f1 (a:GA<'a>) = a.F1;
+    "Text"  |> GA |> f1 |> should equal "Text"
+    1       |> GA |> f1 |> should equal 1
+    ()      |> GA<obj> |> f1 |> should equal ()
+    
+    let ga = 5.5 |> GA
+    1000 |> ga.F2 |> should equal ()
+    //"xx" |> ga.F2<obj> |> should equal ()
+
+    "../../AssemblyInfo.fs" |> FileInfo |> fun f -> f.Extension |> should equal ".fs"
 
 [<Test>]
 let FsRotate() =
