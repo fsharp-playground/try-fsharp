964b8173bea01cf8c8cdf462c68ac172f636c773
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-01-12 00:49:28 +0700

    test checked and unchecked

---
+++ Cs/PrimitiveSpec.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs
+{
+    public class PrimitiveSpec
+    {
+        [Test]
+        public void ShouldPrintMaxValue()
+        {
+            var max = Int32.MaxValue;
+            var min = Int32.MinValue;
+
+            Assert.AreEqual(max, 63);
+            Assert.AreEqual(min, -63);
+        }
+    }
+}
---
+++ Single/Cs/ManualChecked/build.sh
@@ -0,0 +1,3 @@
+
+dmcs CheckedManual.cs -target:library
+monodis --output=CheckedManual.il CheckedManual.dll
